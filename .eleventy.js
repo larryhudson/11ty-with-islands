091e83eaf5ac16ed46b248d2c3427baa944e765e
Larry Hudson <larryhudson@hey.com>
2022-06-12 10:44:11 +1000

    add embedded youtube player

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
     "node_modules/@11ty/is-land/is-land.js": "is-land.js",
   });
 
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/lite-youtube-embed/src": "web-components/lite-youtube-embed",
+  });
+
   return {
     dir: {
       input: "src",
