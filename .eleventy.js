5f3bbdc38395d656753bac9b9067f3ff2cf22ce2
Larry Hudson <larryhudson@hey.com>
2022-06-12 10:18:04 +1000

    initial

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@11ty/is-land/is-land.js": "is-land.js",
+  });
+
   return {
     dir: {
       input: "src",
       output: "_site",
     },
+    markdownTemplateEngine: "njk",
   };
 };
